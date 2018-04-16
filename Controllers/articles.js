6980b0b63bc12a456ed3bb9f26ed168aa9d1d174
wmoffa <apay@rbscon.com>
2018-04-16 15:55:16 -0400

    Add files via upload

---
+++ Controllers/articles.js
@@ -0,0 +1,24 @@
+const db = require("../models");
+
+module.exports = {
+  findAll: function(req, res) {
+    db.Article
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbArticle => res.json(dbArticle))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Article
+      .create(req.body)
+      .then(dbArticle => res.json(dbArticle))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Article
+      .findById({ _id: req.params.id })
+      .then(dbArticle => dbArticle.remove())
+      .then(dbArticle => res.json(dbArticle))
+      .catch(err => res.status(422).json(err));
+  }
+};
