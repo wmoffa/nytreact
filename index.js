9438e6050a8666fbb65f4aab28f901a0a76b9481
wmoffa <apay@rbscon.com>
2018-04-16 15:37:53 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const articlesController = require("../../controllers/articles");
+
+
+app.get("/", function(req, res){
+    res.send();
+    })
+  
+
+
+module.exports = router;
